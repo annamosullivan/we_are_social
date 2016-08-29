45e7b8b7afe59c04ada2fbf15349661a0c92c9c0
Anne <osullivan_anne@hotmail.com>
2016-08-29 23:01:00 +0100

    6th attempt at deployment

--- contact/migrations/0002_feedback_email.py
+++ contact/migrations/0002_feedback_email.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='feedback',
             name='email',
-            field=models.EmailField(default=datetime.datetime(2016, 4, 24, 11, 37, 53, 773000, tzinfo=utc), max_length=20),
+            field=models.EmailField(default='', max_length=20),
             preserve_default=False,
         ),
     ]
