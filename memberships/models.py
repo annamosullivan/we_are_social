1026c522c95596bf863d6c30fca90b23f9aee646
Anne <osullivan_anne@hotmail.com>
2016-11-20 21:06:24 +0000

    tried changing the Paypal sandbox in models.py in memberships to have products show right name in sandbox

--- memberships/models.py
+++ memberships/models.py
@@ -1,5 +1,5 @@
 import uuid
-
+from django import template
 import paypal.standard.forms
 from django.conf import settings
 from django.db import models
@@ -18,10 +18,20 @@ class Membership(models.Model):
             "business": settings.PAYPAL_RECEIVER_EMAIL,
             "amount": self.price,
             "currency": "USD",
-            "item_name": "%s-%s" % (self.pk, uuid.uuid4()),
+            "cmd": "_xclick-subscriptions",
+            "invoice": uuid.uuid4,
+            "item_name": "%s-%s" % (self.name, uuid.uuid4()),
             "notify_url": settings.PAYPAL_NOTIFY_URL,
             "return_url": "%s/paypal-return/" % settings.SITE_URL,
             "cancel_return": "%s/paypal-cancel/" % settings.SITE_URL
         }
 
         return paypal.standard.forms.PayPalPaymentsForm(initial=paypal_dict)
+
+    # render the form
+        if settings.DEBUG:
+            html = PayPalPaymentsForm(initial=paypal_dict,button_type='subscribe').render()
+        else:
+            html = PayPalPaymentsForm(initial=paypal_dict,button_type='subscribe').render()
+        return html
+
