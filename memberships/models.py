f422e951bbee13246d421333d04f97604088f9e1
Anne <osullivan_anne@hotmail.com>
2016-11-20 22:38:29 +0000

    put models.py in memberships app to be back to original code

--- memberships/models.py
+++ memberships/models.py
@@ -18,9 +18,7 @@ class Membership(models.Model):
             "business": settings.PAYPAL_RECEIVER_EMAIL,
             "amount": self.price,
             "currency": "USD",
-            "cmd": "_xclick-subscriptions",
-            "invoice": uuid.uuid4,
-            "item_name": "%s-%s" % (self.name, uuid.uuid4()),
+            "item_name": "%s-%s" % (self.pk, uuid.uuid4()),
             "notify_url": settings.PAYPAL_NOTIFY_URL,
             "return_url": "%s/paypal-return/" % settings.SITE_URL,
             "cancel_return": "%s/paypal-cancel/" % settings.SITE_URL
@@ -28,10 +26,4 @@ class Membership(models.Model):
 
         return paypal.standard.forms.PayPalPaymentsForm(initial=paypal_dict)
 
-    # render the form
-        if settings.DEBUG:
-            html = PayPalPaymentsForm(initial=paypal_dict,button_type='subscribe').render()
-        else:
-            html = PayPalPaymentsForm(initial=paypal_dict,button_type='subscribe').render()
-        return html
 
