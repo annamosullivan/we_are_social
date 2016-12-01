ff683520a4db5b5a0e4c31999f20eeef7b57e63f
Anne <osullivan_anne@hotmail.com>
2016-12-01 22:34:37 +0000

    got rid of self parameter in get_user function in line 201 of accounts/tests.py

--- accounts/tests.py
+++ accounts/tests.py
@@ -198,7 +198,7 @@ class TestBackends(TestCase):
             return user
         return None
 
-    def get_user(self, user_id):
+    def get_user(user_id):
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
